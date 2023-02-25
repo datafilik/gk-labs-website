a9a121d955e049e932c3e29e6e7c7871c9ae7d86
Victor Ocheri <18663485+datafilik@users.noreply.github.com>
2023-02-25 01:06:08 +0000

    added dynamic pages for each job items

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 gkLabsApp = Flask(__name__)
 
 jobs = load_jobs_from_db()  # job list
 
+
 @gkLabsApp.route("/")
 def home():
-  return render_template('home.html', jobs=jobs, company_name='Gutenkraft')
+  return render_template('home.html', jobs=jobs)
 
 
 @gkLabsApp.route("/api/jobs")
 def list_jobs():
   return jsonify(jobs)
+
+
+@gkLabsApp.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+
+  return render_template('jobpage.html', job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -17,10 +17,19 @@ engine = create_engine(db_connection_uri,
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-    query = "select * from jobs"
-    result = conn.execute(text(query))
+    result = conn.execute(text("SELECT * FROM jobs"))
 
     jobs = []
     for row in result.all():
       jobs.append(dict(row._mapping))
     return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = {}".format(id)))
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0]._mapping)
