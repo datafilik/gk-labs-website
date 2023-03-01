447309429061ce3724f04f6a19f2fee819bfd726
Victor Ocheri <18663485+datafilik@users.noreply.github.com>
2023-03-01 01:52:42 +0000

    added capability to store form data to database.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 gkLabsApp = Flask(__name__)
 
@@ -23,6 +23,14 @@ def show_job(id):
     return "Not Found", 404
 
   return render_template('jobpage.html', job=job)
+
+
+@gkLabsApp.route("/job/<id>/apply", methods=['post'])
+def apply_for_job(id):
+  data = request.form  # request.args used for GET requests
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)  # store application data to database
+  return render_template('applicationsubmitted.html', form_data=data, job=job)
 
 
 if __name__ == "__main__":
