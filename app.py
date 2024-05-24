02c850fe702b112b54407f3e56dbbbf0472003cc
Victor Ocheri <datafilik@gmail.com>
2024-05-24 13:14:24 +0100

    temporarily detached Planetscale DB

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
-from database import load_jobs_from_db, load_job_from_db, add_application_to_db
+# from database import load_job_from_db, add_application_to_db
+from database import load_jobs_from_db
 from kaito import process_prompt
 import os
 
@@ -90,33 +91,33 @@ def career():
   return render_template('careers.html', jobs=jobs)
 
 
-@gkLabsApp.route("/careers/job/<id>")
-def show_job(id):
-  job = load_job_from_db(id)
-  if not job:
-    return "Not Found", 404
-  return render_template('jobpage.html', job=job)
+# @gkLabsApp.route("/careers/job/<id>")
+# def show_job(id):
+#   job = load_job_from_db(id)
+#   if not job:
+#     return "Not Found", 404
+#   return render_template('jobpage.html', job=job)
 
 
-@gkLabsApp.route("/careers/job/<id>/apply", methods=['post'])
-def apply_for_job(id):
-  data = request.form  # request.args used for GET requests
-  job = load_job_from_db(id)
-  add_application_to_db(id, data)  # store application data to database
-  return render_template('applicationsubmitted.html', form_data=data, job=job)
+# @gkLabsApp.route("/careers/job/<id>/apply", methods=['post'])
+# def apply_for_job(id):
+#   data = request.form  # request.args used for GET requests
+#   job = load_job_from_db(id)
+#   add_application_to_db(id, data)  # store application data to database
+#   return render_template('applicationsubmitted.html', form_data=data, job=job)
 
 
-@gkLabsApp.route("/api/careers/jobs")
-def list_jobs():
-  return jsonify(jobs)
+# @gkLabsApp.route("/api/careers/jobs")
+# def list_jobs():
+#   return jsonify(jobs)
 
 
-@gkLabsApp.route("/api/careers/job/<id>")
-def get_job_data(id):
-  job = load_job_from_db(id)
-  if not job:
-    return "Not Found", 404
-  return jsonify(job)
+# @gkLabsApp.route("/api/careers/job/<id>")
+# def get_job_data(id):
+#   job = load_job_from_db(id)
+#   if not job:
+#     return "Not Found", 404
+#   return jsonify(job)
 
 
 @gkLabsApp.route("/ops")
--- database.py
+++ database.py
@@ -5,58 +5,53 @@ from dotenv import load_dotenv
 # load environment variable from .env file
 load_dotenv()
 
-# host = os.getenv("DB_HOST")
-# user = os.getenv("DB_USERNAME")
-# passwd = os.getenv("DB_PASSWORD")
-# db_name = os.getenv("DB_NAME")
-
 host = os.environ["DB_HOST"]
 user = os.environ["DB_USERNAME"]
 passwd = os.environ["DB_PASSWORD"]
 db_name = os.environ["DB_NAME"]
 
-db_connection_uri = "mysql+pymysql://{}:{}@{}/{}?charset=utf8mb4".format(
-  user, passwd, host, db_name)
+# db_connection_uri = "mysql+pymysql://{}:{}@{}/{}?charset=utf8mb4".format(
+#   user, passwd, host, db_name)
 
-engine = create_engine(db_connection_uri,
-                       connect_args={"ssl": {
-                         "ssl_ca": "/etc/ssl/cert.pem"
-                       }})
+# engine = create_engine(db_connection_uri,
+#                        connect_args={"ssl": {
+#                          "ssl_ca": "/etc/ssl/cert.pem"
+#                        }})
 
 
 def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM jobs"))
+  jobs = []
+  # with engine.connect() as conn:
+  #   result = conn.execute(text("SELECT * FROM jobs"))
 
-    jobs = []
-    for row in result.all():
-      jobs.append(dict(row._mapping))
-    return jobs
-
-
-def load_job_from_db(id):
-  with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM jobs WHERE id = {}".format(id)))
-    rows = result.all()
-    if len(rows) == 0:
-      return None
-    else:
-      return dict(rows[0]._mapping)
+  #   for row in result.all():
+  #     jobs.append(dict(row._mapping))
+  return jobs
 
 
-def add_application_to_db(job_id, data):
-  with engine.connect() as conn:
-    query = text(
-      "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
-    )
+# def load_job_from_db(id):
+#   with engine.connect() as conn:
+#     result = conn.execute(text("SELECT * FROM jobs WHERE id = {}".format(id)))
+#     rows = result.all()
+#     if len(rows) == 0:
+#       return None
+#     else:
+#       return dict(rows[0]._mapping)
 
-    conn.execute(
-      query, {
-        "job_id": job_id,
-        "full_name": data['full_name'],
-        "email": data['email'],
-        "linkedin_url": data['linkedin_url'],
-        "education": data['education'],
-        "work_experience": data['work_experience'],
-        "resume_url": data['resume_url']
-      })
+
+# def add_application_to_db(job_id, data):
+#   with engine.connect() as conn:
+#     query = text(
+#       "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+#     )
+
+#     conn.execute(
+#       query, {
+#         "job_id": job_id,
+#         "full_name": data['full_name'],
+#         "email": data['email'],
+#         "linkedin_url": data['linkedin_url'],
+#         "education": data['education'],
+#         "work_experience": data['work_experience'],
+#         "resume_url": data['resume_url']
+#       })
